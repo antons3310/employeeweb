eaa5d8a265fd1efd5bfeb98c4bb0803d62b7126b
antoshka <ashebalkin@diasoft.ru>
2021-12-01 23:00:09 +0300

    Заготовка для сдачи домашнего задания по исключениям

---
+++ src/main/java/ru/ashebalkin/skypro/course2/lesson2/employeeweb/EmployeewebApplication.java
@@ -0,0 +1,13 @@
+package ru.ashebalkin.skypro.course2.lesson2.employeeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeewebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeewebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ashebalkin/skypro/course2/lesson2/employeeweb/EmployeewebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ashebalkin.skypro.course2.lesson2.employeeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeewebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
